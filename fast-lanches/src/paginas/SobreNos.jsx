9ef5d2c32a8e4f6c3dcd8966de9bd845b4cc9399
ferreiravsk-ctrl <ferreiravsk@gmail.com>
2026-06-05 23:01:40 -0300

    add page/componente sobreNos

---
+++ fast-lanches/src/paginas/SobreNos.jsx
@@ -0,0 +1,17 @@
+function SobreNos() {
+  return (
+    <>
+      <h2 id="titulosobre">Sobre Nós</h2>
+
+      <p id="org">
+        A Fast Lanches nasceu da paixão pela culinária, pela dedicação ao trabalho em equipe e pela busca constante pela excelência.Nossa história é marcada por desafios, aprendizado e pela vontade de transformar uma simples hamburgueria em um lugar especial para nossos clientes.
+        Tudo começou com o sonho de oferecer hambúrgueres artesanais preparados com ingredientes selecionados, receitas autênticas e um atendimento que faz cada cliente se sentir em casa. Acreditamos que o sucesso é construído diariamente através do esforço, da organização e do comprometimento de cada integrante da equipe.
+        Na Fast Lanches, cada hambúrguer é preparado com atenção aos detalhes, combinando sabor, qualidade e criatividade. Nosso objetivo é proporcionar uma experiência única, unindo o ambiente acolhedor de uma lanchonete tradicional com a inovação e a paixão pela gastronomia moderna.
+        Mais do que servir refeições, buscamos criar momentos especiais. Valorizamos o respeito, a colaboração e a dedicação, princípios que nos inspiram a evoluir continuamente e entregar o melhor para nossos clientes todos os dias.
+        Seja para um lanche rápido, um encontro entre amigos ou uma refeição em família, a Fast Lanches está sempre pronta para receber você com muito sabor, qualidade e carinho.
+      </p>
+    </>
+  );
+}
+
+export default SobreNos;
