8f42a7348a1096015166caad5eedc6efa7da1384
ferreiravsk-ctrl <ferreiravsk@gmail.com>
2026-06-05 23:17:35 -0300

    add componente entregaFlutuante

---
+++ fast-lanches/src/componentes/EntregaFlutuante.jsx
@@ -0,0 +1,23 @@
+function EntregaFlutuante() {
+  return (
+    <div id="entrega">
+      <div id="tel">
+        <a href="tel:61995059763">
+          <h4>(61) 99505-9763</h4>
+        </a>
+
+        <h5>
+          Entregamos na sua casa!
+        </h5>
+      </div>
+
+      <img
+        id="tamanhotel"
+        src="/images/tel.png"
+        alt="Telefone"
+      />
+    </div>
+  );
+}
+
+export default EntregaFlutuante;
